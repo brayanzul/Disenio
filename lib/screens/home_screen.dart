b1d765ae19550829b156f2dec1ad55e33c753b81
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2023-01-01 10:48:56 -0500

    Add Design Project and today work on the Home Screen, Background

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+
+import 'package:disenio/widgets/background.dart';
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          
+          // Background
+          Background(),
+
+        ],
+      )
+   );
+  }
+}
